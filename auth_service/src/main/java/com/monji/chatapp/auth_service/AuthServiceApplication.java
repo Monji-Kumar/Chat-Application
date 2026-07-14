ba021316b77e4e3b10e1b5f2ceabc446fb1ad109
Monji Kumar <workwithmonji@gmail.com>
2026-07-14 17:04:41 +0530

    Update local service startup configuration

--- auth_service/src/main/java/com/monji/chatapp/auth_service/AuthServiceApplication.java
+++ auth_service/src/main/java/com/monji/chatapp/auth_service/AuthServiceApplication.java
@@ -3,10 +3,13 @@ package com.monji.chatapp.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(scanBasePackages = "com.monji.chatapp")
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
--- chat_service/src/main/java/com/monji/chatapp/chat_service/ChatServiceApplication.java
+++ chat_service/src/main/java/com/monji/chatapp/chat_service/ChatServiceApplication.java
@@ -3,10 +3,13 @@ package com.monji.chatapp.chat_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(ChatServiceApplication.class, args);
 	}
 
